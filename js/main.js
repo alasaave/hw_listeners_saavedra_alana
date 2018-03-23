8cbdd64b404253177dda8f9c7bf44e2825001cce
Alana Saavedra <alasaave@gmail.com>
2018-03-23 16:32:23 -0500

    create keydown event listener

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//Scroll past 150px
+//Scroll: image has purple shadow past 150px
 window.addEventListener('scroll', function () {
     if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
         document.getElementById('headerImage').className = 'changePic';
@@ -7,20 +7,17 @@ window.addEventListener('scroll', function () {
     }
 });
 
-/*Click
-var pageHeading = document.getElementById('pageHeading');
-
-pageHeading.addEventListener('click', listener, false);
-
-function listener() {
-    document.getElementById('show').innerHTML = '<h1>Please watch</h1>';
-}
-*/
-
-//Click 2
+//Click: shows <h1> @ top of page
 var pageHeading = document.getElementById('pageHeading');
 
 pageHeading.addEventListener('click', function () {
     document.getElementById('show').innerHTML = '<h1>Please watch</h1>';
 }
 );
+
+//Keydown
+document.getElementById('finish').addEventListener('keydown', footerImg);
+
+function footerImg() {
+    document.getElementById('finish').style.backgroundImage = "url('img/clap.gif')";
+}
